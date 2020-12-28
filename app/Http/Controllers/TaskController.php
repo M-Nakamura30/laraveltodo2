594ea74cea8ee6f70d011470bbafdf6c3c0a394f
masato.nakamura <masato.nakamura30@gmail.com>
2020-12-28 20:10:51 +0900

    tutorial04_finishi

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,22 +3,37 @@
 namespace App\Http\Controllers;
 
 use App\Folder;
+use App\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
 	//$idを受け取って
+	//フォルダとそれに基づくタスクを取得する
     public function index(int $id)
     {
-    	//フォルダデータをデータベースから取得
+    	//全てのフォルダを取得する
     	$folders = Folder::all();
 
-    	//$foldersでviewに渡す
+    	//選ばれたフォルダを取得
+    	$current_folder = Folder::find($id);
+
+    	//選ばれたフォルダに紐づくタスクを取得
+    	//フォルダIDに基づくを取得
+    	//第一引数がカラム名。第二引数が比較する値
+    	
+    	//外部キー
+    	// $tasks = Task::where('folder_id', $current_folder->id)->get();
+    	//Tasks::where('folder_id', '=', $current_folder->id)->get();
+    	$tasks = $current_folder->tasks()->get();
+
+    	//フォルダ、フォルダID、タスクをviewに渡す
     	return view('tasks/index', [
     		'folders' => $folders,
     		//フォルダ名を色分けして判断するためのもの
     		//ルーティングで定義したカッコと同じもの
-    		'current_folder_id' => $id,
+    		'current_folder_id' => $current_folder->id,
+    		'tasks' => $tasks,
     	]);
     }
 }
